06f30116787e651d59fb77f8cb42be6a879a537d
Regina Azzi <reginaazzi@hotmail.com>
2023-02-03 12:11:00 -0300

    ADD

---
+++ src/exercicios/Atribuicao.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+public class Atribuicao {
+
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = a;
+		int c = a + b ;
+		
+		c += b; // mesma coisa c = c + b;
+		c -= a; // mesma coisa c = c - a;
+		c *= b; // mesma coisa c = c * b;
+		c /= a; // mesma coisa c = c / a;
+		
+		
+		c++; // c = c + 1;
+		System.out.println(c);
+		
+		c %= 2; // c = c % 2; 0 ou 1;
+		
+		System.out.println(c);
+		
+
+	}
+
+}
